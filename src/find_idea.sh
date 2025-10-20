9e9356e0b8e987ebcdabd862fb2228ce77318ecf
LudoO <ludovic.valente@gmail.com>
2025-10-20 16:06:08 +0200

    chore: cleanup

--- src/find_idea.sh
+++
@@ -1,6 +0,0 @@
-IDEA=`find "$HOME/Applications/" -name idea | tail -n1`
-if [ -z "$IDEA" ]
-then
-IDEA=`find ~/"Library/Application Support/JetBrains/" -name idea | tail -n1`
-fi
-echo "Open IntelliJ...  $IDEA"
